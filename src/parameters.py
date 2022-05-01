e66399928e6b1772c42ad8bdd385f8a5e15f614d
hobs <hobs@totalgood.com>
2022-05-01 15:42:19 -0700

    modularize

---
+++ src/parameters.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False)
+class Parameters:
+    seq_len: int = 35
+    num_words: int = 2000
+
+    # Model parameters
+    embedding_size: int = 64
+    out_size: int = 32
+    stride: int = 2
+
+    # Training parameters
+    epochs: int = 10
+    batch_size: int = 12
+    learning_rate: float = 0.001
